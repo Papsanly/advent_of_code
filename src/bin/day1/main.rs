2781053548f2d349e7dfab27f63b6ac91e0be924
Papsan <papsanlysenko@gmail.com>
2023-12-05 04:45:41 +0200

    rename

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,13 +1,11 @@
-mod game;
 mod part1;
 mod part2;
-
 use part1::part1;
 use part2::part2;
 
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Part 1: {:?}", part1());
-    println!("Part 2: {:?}", part2());
+    println!("Part day1: {:?}", part1());
+    println!("Part day2: {:?}", part2());
 }
