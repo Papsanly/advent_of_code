9c80df9d214153ef51ba63307093acf0f20b4b68
Papsan <papsanlysenko@gmail.com>
2023-12-05 04:44:44 +0200

    finished part 2 for day 2

--- src/bin/2/part2.rs
+++ src/bin/2/part2.rs
@@ -1,1 +1,27 @@
-pub fn part2() {}
+use crate::game::Game;
+use crate::INPUT;
+
+macro_rules! max_by_color {
+    ($game:ident, $color:ident) => {
+        $game
+            .sets
+            .iter()
+            .max_by_key(|set| set.$color.unwrap_or(0))?
+            .$color?
+    };
+}
+
+pub fn part2() -> Option<usize> {
+    let games = INPUT
+        .lines()
+        .map(str::parse)
+        .map(Result::ok)
+        .collect::<Option<Vec<Game>>>()?;
+
+    games
+        .iter()
+        .map(|game| {
+            Some(max_by_color!(game, red) * max_by_color!(game, green) * max_by_color!(game, blue))
+        })
+        .sum()
+}
